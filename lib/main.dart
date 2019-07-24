1b7a84d47719de2c7e6c648258bdd712f1f7f519
Mugi <mugypleci@gmail.com>
2019-07-24 14:48:34 +0700

    Send data to a new screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
+class Todo{
+  final String title;
+  final String description;
+
+  Todo(this.title, this.description);
+}
+
+void main(){
+  runApp(MaterialApp(
+    title: 'Passing Data',
+    home: TodosScreen(
+      todos: List.generate(
+        20,
+        (i) => Todo(
+          'Todo $i',
+          'A description of what needs to be done fot todo $i'
+        ),
+      ),
+    ),
+    ));
+}
+
+class TodosScreen extends StatelessWidget {
+  final List<Todo> todos;
+
+  TodosScreen({Key key, @required this.todos}) : super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todos'),
+      ),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index){
+          return ListTile(
+            title: Text(todos[index].title),
+            //when a user taps the ListTitle to the DetailScreen
+            //Notice that youre not only creating a DetailScreen youre
+            //also passing the current todo through to it
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DetailScreen(todo: todos[index]),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  //declare a field that holds the todo
+  final Todo todo;
+
+  //In the constructor, require a todo
+  DetailScreen({Key key, @required this.todo}) : super (key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(todo.title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text(todo.description),
+      ),
+    );
+  }
+}
