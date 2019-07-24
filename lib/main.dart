66ca0065f9fde79c9ffcdb032225f061f68534a8
Mugi <mugypleci@gmail.com>
2019-07-24 13:33:05 +0700

    Pass arguments to a named route

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,99 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      onGenerateRoute: (settings){
+        if(settings.name == PassArgumentsScreen.routeName){
+          final ScreenArguments args = settings.arguments;
+
+          return MaterialPageRoute(
+            builder: (context) {
+              return PassArgumentsScreen(
+                title: args.title,
+                message: args.message,
+              );
+            },
+          );
+        }
+      },
+    title: 'navigation with Arguments',
+    home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                child: Text('Navigate to screen that extracts arguments'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ExtractArgumentsScreen(),
+                      settings: RouteSettings(
+                        arguments: ScreenArguments( 
+                          'Extract Arguments Screen',
+                          'This message is extracted in the build method'
+                        ),
+                      ), 
+                    ),
+                  );
+                },
+              ),
+              RaisedButton(
+                child: Text('Navigate to a named that accepts arguments'),
+                onPressed: () {
+                  Navigator.pushNamed(
+                    context, 
+                    PassArgumentsScreen.routeName,
+                    arguments: ScreenArguments(
+                      'Accept Arguments Sceen',
+                      'This message is extracted in the onGenerate Route function',
+                    ),
+                  );
+                },
+              )
+            ],
+          ),
+        ),
+    );
+  }
+}
+
+class ExtractArgumentsScreen extends StatelessWidget {
+  static const routeName  = 'ExtractArguments';
+
+  @override
+  Widget build(BuildContext context) {
+    final ScreenArguments args  = ModalRoute.of(context).settings.arguments;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(args.title),
+        ),
+        body: Center(
+          child: Text(args.message),
+        ),
+    );
+  }
+}
+
+
 class PassArgumentsScreen extends StatelessWidget {
   static const routeName = '/passArguments';
 
